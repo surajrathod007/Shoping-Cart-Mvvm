85a5006066cfb422f7a602144ff8d6c4f4f35fb1
Suraj <surajsinhrathod75@gmail.com>
2022-09-15 08:40:44 +0530

    pdf invoice added

--- app/src/main/java/com/surajrathod/shopingcart/ui/CartFragment.kt
+++ app/src/main/java/com/surajrathod/shopingcart/ui/CartFragment.kt
@@ -1,15 +1,29 @@
 package com.surajrathod.shopingcart.ui
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DividerItemDecoration
+import com.itextpdf.commons.utils.DateTimeUtil
+import com.itextpdf.kernel.colors.Color
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Cell
+import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Table
+import com.itextpdf.layout.element.Text
 import com.surajrathod.shopingcart.R
 import com.surajrathod.shopingcart.adapters.CartAdapter
 import com.surajrathod.shopingcart.databinding.FragmentCartBinding
@@ -20,6 +34,8 @@ import com.surajrathod.shopingcart.repo.CartRepo
 import com.surajrathod.shopingcart.repo.ShopRepo
 import com.surajrathod.shopingcart.viewmodels.ShopViewModel
 import com.surajrathod.shopingcart.viewmodels.ShopViewModelFactory
+import java.io.File
+import java.io.FileOutputStream
 
 
 class CartFragment : Fragment() , CartAdapter.CartInterface{
@@ -73,6 +89,10 @@ class CartFragment : Fragment() , CartAdapter.CartInterface{
             navController.navigate(R.id.action_cartFragment_to_orderFragment)
         }
 
+        binding.generateBill.setOnClickListener {
+            generatePdf()
+        }
+
 
     }
 
@@ -84,6 +104,80 @@ class CartFragment : Fragment() , CartAdapter.CartInterface{
     override fun changeQty(cartItem: CartItem, qty: Int) {
         viewModel.cartRepo.changeQuantity(cartItem,qty)
         //Toast.makeText(requireContext(),"$qty Deleted",Toast.LENGTH_SHORT).show()
+    }
+
+    fun generatePdf(){
+
+        try{
+
+            var lst = viewModel.getCart().value
+
+            val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString()
+            val file = File(path,"mypdf.pdf")
+            val output = FileOutputStream(file)
+
+            val writer = PdfWriter(file)
+            val pdfDocument = PdfDocument(writer)
+            val document = Document(pdfDocument)
+
+
+            val paragraph = Paragraph("Hey there ! Mann Sign\n\nInvoice")
+            //paragraph.add(DateTimeUtil.getCurrentTimeDate().toString())
+            document.add(paragraph)
+
+
+            val table = Table(3)
+            table.addCell("Product")
+            table.addCell("Qty")
+            table.addCell("Price ")
+
+            lst!!.forEach {
+                table.addCell(it.product.name)
+                table.addCell(it.qty.toString())
+                table.addCell((it.product.price*it.qty).toString())
+            }
+
+            table.addCell("")
+            val total = viewModel.getTotalPrice()?.value ?: 0.0
+            table.addCell("Sub Total : ")
+            table.addCell("$total")
+
+            table.addCell("")
+            table.addCell("Gst : 18% ")
+            table.addCell("-${(total*18)/100}")
+            table.addCell("")
+            table.addCell("Grand Total : ")
+            table.addCell("$${total-(total*18)/100}")
+
+
+
+            document.add(table)
+            document.add(Paragraph("\n\nBill Genereted : "+ DateTimeUtil.getCurrentTimeDate().toString()))
+
+            document.close()
+            Toast.makeText(this.requireContext(),"Pdf Created",Toast.LENGTH_SHORT).show()
+            openFile(file,path)
+
+        }catch ( e : Exception){
+            Toast.makeText(this.requireContext(),e.message,Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun openFile(file : File,path : String){
+        val intent = Intent(Intent.ACTION_VIEW)
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.N){
+            val uri = FileProvider.getUriForFile(this.requireActivity(),this.requireActivity().packageName+".provider",file)
+            intent.setData(uri)
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            startActivity(intent)
+        }else{
+            intent.setDataAndType(Uri.parse(path),"application/pdf")
+            val i = Intent.createChooser(intent,"Open File With")
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(i)
+        }
     }
 
 
--- app/src/main/java/com/surajrathod/shopingcart/ui/MainActivity.kt
+++ app/src/main/java/com/surajrathod/shopingcart/ui/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.surajrathod.shopingcart.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Paragraph
 import com.surajrathod.shopingcart.R
 import com.surajrathod.shopingcart.repo.CartRepo
 import com.surajrathod.shopingcart.repo.ShopRepo
 import com.surajrathod.shopingcart.viewmodels.ShopViewModel
 import com.surajrathod.shopingcart.viewmodels.ShopViewModelFactory
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,12 +41,26 @@ class MainActivity : AppCompatActivity() {
     val cartRepo = CartRepo()
     var cartQty = 0
 
+    var isRead = false
+    var isWrite = false
+
+    lateinit var permissionLauncher : ActivityResultLauncher<Array<String>>
+
+
     val TAG = "Suraj"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            isRead = it[Manifest.permission.READ_EXTERNAL_STORAGE] ?: isRead
+            isWrite = it[Manifest.permission.WRITE_EXTERNAL_STORAGE] ?: isWrite
+        }
+
+        requestPermission()
         navController = Navigation.findNavController(this,R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
 
@@ -42,6 +69,7 @@ class MainActivity : AppCompatActivity() {
 //        shopViewModel.getTotalPrice()?.observe(this,{
 //            Toast.makeText(this@MainActivity,"Total is $it",Toast.LENGTH_SHORT).show()
 //        })
+
 
 
         //just press alt+enter on above code , so lamda will be out of parenthis
@@ -91,5 +119,31 @@ class MainActivity : AppCompatActivity() {
 
 
         return super.onCreateOptionsMenu(menu)
+    }
+
+
+
+    fun requestPermission(){
+
+        //check permission already granted or not
+        isRead = ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        isWrite = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+        var permissionRequest : MutableList<String> = ArrayList()
+
+        if(!isRead){
+            permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        if(!isWrite){
+            permissionRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if(permissionRequest.isNotEmpty()){
+            //request permission
+            permissionLauncher.launch(permissionRequest.toTypedArray())
+        }
+
+
     }
 }
