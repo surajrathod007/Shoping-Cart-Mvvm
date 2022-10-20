14638c360e49cd1de76a6c9484d0a6a45d4ee7a9
Suraj <surajsinhrathod75@gmail.com>
2022-10-20 22:10:29 +0530

    Mann Invoice First Table Added

--- app/src/main/java/com/surajrathod/shopingcart/ui/CartFragment.kt
+++ app/src/main/java/com/surajrathod/shopingcart/ui/CartFragment.kt
@@ -1,6 +1,8 @@
 package com.surajrathod.shopingcart.ui
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -10,20 +12,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.itextpdf.commons.utils.DateTimeUtil
+import com.itextpdf.io.image.ImageDataFactory
 import com.itextpdf.kernel.colors.Color
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.layout.Document
-import com.itextpdf.layout.element.Cell
-import com.itextpdf.layout.element.Paragraph
-import com.itextpdf.layout.element.Table
-import com.itextpdf.layout.element.Text
+import com.itextpdf.layout.Style
+import com.itextpdf.layout.element.*
+import com.itextpdf.layout.properties.TextAlignment
 import com.surajrathod.shopingcart.R
 import com.surajrathod.shopingcart.adapters.CartAdapter
 import com.surajrathod.shopingcart.databinding.FragmentCartBinding
@@ -34,8 +38,11 @@ import com.surajrathod.shopingcart.repo.CartRepo
 import com.surajrathod.shopingcart.repo.ShopRepo
 import com.surajrathod.shopingcart.viewmodels.ShopViewModel
 import com.surajrathod.shopingcart.viewmodels.ShopViewModelFactory
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.time.LocalDate
+import java.util.*
 
 
 class CartFragment : Fragment() , CartAdapter.CartInterface{
@@ -62,6 +69,7 @@ class CartFragment : Fragment() , CartAdapter.CartInterface{
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -90,7 +98,7 @@ class CartFragment : Fragment() , CartAdapter.CartInterface{
         }
 
         binding.generateBill.setOnClickListener {
-            generatePdf()
+            makeInvoice()
         }
 
 
@@ -113,7 +121,7 @@ class CartFragment : Fragment() , CartAdapter.CartInterface{
             var lst = viewModel.getCart().value
 
             val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString()
-            val file = File(path,"mypdf.pdf")
+            val file = File(path,"mann_invoice${System.currentTimeMillis()}.pdf")
             val output = FileOutputStream(file)
 
             val writer = PdfWriter(file)
@@ -152,6 +160,101 @@ class CartFragment : Fragment() , CartAdapter.CartInterface{
 
 
             document.add(table)
+            document.add(Paragraph("\n\nBill Genereted : "+ DateTimeUtil.getCurrentTimeDate().toString()))
+
+            document.close()
+            Toast.makeText(this.requireContext(),"Pdf Created",Toast.LENGTH_SHORT).show()
+            openFile(file,path)
+
+        }catch ( e : Exception){
+            Toast.makeText(this.requireContext(),e.message,Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun makeInvoice(){
+
+        try{
+
+            var lst = viewModel.getCart().value
+
+            val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString()
+            val file = File(path,"mann_invoice${System.currentTimeMillis()}.pdf")
+            val output = FileOutputStream(file)
+
+            val writer = PdfWriter(file)
+            val pdfDocument = PdfDocument(writer)
+            val document = Document(pdfDocument)
+
+
+            //header
+            val headerImg = requireContext().getDrawable(R.drawable.invoice_header)
+            val bitmap = (headerImg as BitmapDrawable).bitmap
+            val opstream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG,100,opstream)
+            val bitmapdata = opstream.toByteArray()
+
+            val img = ImageDataFactory.create(bitmapdata)
+            val myheader = Image(img)
+            document.add(myheader)
+
+
+            //first table
+            val c : FloatArray = floatArrayOf(220F, 220F, 200F, 180F)
+            val table1 = Table(c)
+
+
+            // row 1
+            table1.addCell(Cell().add(Paragraph("Bill to Party ").setFontSize(10.0f)).setBold())
+            table1.addCell(Cell().add(Paragraph("Ship to Party ").setFontSize(10.0f)).setBold())
+            table1.addCell(Cell().add(Paragraph("Date : ").setFontSize(10.0f)).setBold())
+            table1.addCell(Cell().add(Paragraph(LocalDate.now().toString()).setFontSize(10.0f).setBold()))
+
+            //row 2
+            table1.addCell(Cell(4,0).add(Paragraph("857,indiranagar -2 ").setFontSize(8.0f)))
+            table1.addCell(Cell(4,0).add(Paragraph("11, My Queen").setFontSize(8.0f)))
+            table1.addCell(Cell().add(Paragraph("Invoice No : ").setFontSize(10.0f)))
+            table1.addCell(Cell().add(Paragraph("inv1000").setFontSize(10.0f)))
+
+            //row 3
+            //table1.addCell(Cell().add(Paragraph("")))
+            //table1.addCell(Cell().add(Paragraph("")))
+            table1.addCell(Cell().add(Paragraph("Buyer's Order No. :").setFontSize(10.0f)))
+            table1.addCell(Cell().add(Paragraph("order97777").setFontSize(10.0f)))
+
+            //row 4
+            //table1.addCell(Cell().add(Paragraph("")))
+            //table1.addCell(Cell().add(Paragraph("")))
+            table1.addCell(Cell().add(Paragraph("Order Date : ").setFontSize(10.0f)))
+            table1.addCell(Cell().add(Paragraph("${LocalDate.now().plusDays(7)}").setFontSize(10.0f)))
+
+            //row 5
+            //table1.addCell(Cell().add(Paragraph("")))
+            //table1.addCell(Cell().add(Paragraph("")))
+            table1.addCell(Cell().add(Paragraph("Status Code : ").setFontSize(10.0f)))
+            table1.addCell(Cell().add(Paragraph("24").setFontSize(10.0f)))
+
+            //row 6
+            table1.addCell(Cell().add(Paragraph("State Code : ").setFontSize(8.0f).setBold()))
+            table1.addCell(Cell().add(Paragraph("State Code : ").setFontSize(8.0f).setBold()))
+            table1.addCell(Cell(0,2).add(Paragraph("COMPANY GSTIN NO : ").setFontSize(8.0f).setBold()).setTextAlignment(TextAlignment.CENTER))
+            //table1.addCell(Cell().add(Paragraph("")))
+
+            //row 7
+            table1.addCell(Cell().add(Paragraph("GSTIN NO : <add here>").setFontSize(8.0f).setBold()))
+            table1.addCell(Cell().add(Paragraph("GSTIN NO : <add here>").setFontSize(8.0f).setBold()))
+            table1.addCell(Cell(0,2).add(Paragraph("24BENPP0006B1Z4").setFontSize(8.0f).setBold()).setTextAlignment(TextAlignment.CENTER))
+            //table1.addCell(Cell().add(Paragraph("")))
+
+
+
+
+
+
+
+            document.add(table1)
             document.add(Paragraph("\n\nBill Genereted : "+ DateTimeUtil.getCurrentTimeDate().toString()))
 
             document.close()
